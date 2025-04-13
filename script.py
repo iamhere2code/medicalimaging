965a7b4694893f7e73c2156ff3397666a21f9593
alishaf18 <alishafaraz@gmail.com>
2025-04-12 21:57:21 -0700

    added preprocessing

--- script.py
+++ script.py
@@ -1,9 +1,49 @@
-# Importing the OpenCV library
-import cv2
-# Reading the image using imread() function
-image = cv2.imread('images/nine_dogs.jpg')   
-output = image.copy()
+# Import statements
+import os
+import keras
+import matplotlib.pyplot as plt
+import numpy as np
 
-# Using the rectangle() function to create a rectangle.
-text = cv2.putText(output, 'OpenCV Demo', (500, 550),
-                cv2.FONT_HERSHEY_SIMPLEX, 4, (255, 0, 0), 2)
+# Clean up the data
+
+# Set the number to skipped
+num_skipped = 0
+# Iterating through the different folder names
+for folder_name in ("glioma", "meningioma", "notumor", "pituitary"):
+    # Joining the paths for each folder 
+    folder_path = os.path.join("/Users/alishuf/Downloads/brain-tumor-mri-dataset/Training", folder_name)
+    for fname in os.listdir(folder_path):
+        fpath = os.path.join(folder_path, fname)
+        try:
+            fobj = open(fpath, "rb")
+            is_jfif = b"JFIF" in fobj.peek(10)
+        finally:
+            fobj.close()
+        # Deleting the file if its not cleaned
+        if not is_jfif:
+            num_skipped += 1
+            # Delete corrupted image
+            os.remove(fpath)
+
+print(f"Deleted {num_skipped} images.")
+
+image_size = (180, 180) # Setting a consistent image size
+batch_size = 128 # How many training examples used in one iterations of training
+
+train_ds, val_ds = keras.utils.image_dataset_from_directory(
+    "/Users/alishuf/Downloads/brain-tumor-mri-dataset/Training",
+    validation_split=0.2,
+    subset="both",
+    seed=1337, # Randomizing the data
+    image_size=image_size,
+    batch_size=batch_size,
+)
+
+# Setting the display size for each of the pictures
+plt.figure(figsize=(10, 10))
+for images, labels in train_ds.take(1):
+    for i in range(9): # Setting the range of images to displat
+        ax = plt.subplot(3, 3, i + 1)
+        plt.imshow(np.array(images[i]).astype("uint8"))
+        plt.title(int(labels[i]))
+        plt.axis("off")
